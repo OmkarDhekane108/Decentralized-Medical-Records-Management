f7a198cdf334f56527625f8ebc6a7903dfaf7472
Omkar Dhekane <mr.omkardhekane@gmail.com>
2026-07-13 04:12:36 +0530

    fixed unresolved merge conflict markers in Authentication.java

--- Authentication.java
+++ Authentication.java
@@ -1,5 +1,10 @@
-<<<<<<< HEAD
-// tumcha local code
-=======
-// remote (origin/main) cha code
->>>>>>> ...
+public class Authentication {
+
+    public static void login() {
+        System.out.println("User Login");
+    }
+
+    public static void logout() {
+        System.out.println("User Logout");
+    }
+}
