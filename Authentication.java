c78bf4bbc3ab1aa2bde9ce4d564783e8c8dfb942
Omkar Dhekane <mr.omkardhekane@gmail.com>
2026-07-13 02:45:37 +0530

    resolved conflict after pulling remote changes

--- Authentication.java
+++ Authentication.java
@@ -1,6 +1,10 @@
-Blockchain.java
-Block.java
-DatabaseConnection.java
-Patient.java
-Doctor.java
-MedicalRecord.java
+public class Authentication {
+
+       public static void login() {
+           System.out.println("User Login");
+       }
+
+       public static void logout() {
+           System.out.println("User Logout");
+       }
+   }
